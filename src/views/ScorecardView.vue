29fabff358e30a446d91c7e9c6cdbc165dd4b3e5
Kushan Shamika <shamikakushan@gmail.com>
2022-06-23 12:02:49 +0530

    🗃️ Scorecard integration

--- src/views/ScorecardView.vue
+++ src/views/ScorecardView.vue
@@ -23,22 +23,33 @@
           color="orange accent-3"
           right
           background-color="transparent"
-          
+          v-model="tab"
         >
-          <v-tab>MCG</v-tab>
-          <v-tab>RCG</v-tab>
+          <v-tab
+            v-if="first_batting_team"
+          >
+            {{first_batting_team.toUpperCase()}}
+          </v-tab>
+          <v-tab
+            v-if="second_batting_team"
+          >
+            {{second_batting_team.toUpperCase()}}
+          </v-tab>
 
-          <v-tab-item>
-            <v-row class="pb-5">
-              <v-col md="6" class="mx-auto">
+          <v-tabs-items v-model="tab">
+            <v-tab-item
+              v-if="first_batting_team"
+            >
+              <v-row class="pb-5">
+                <v-col md="6" class="mx-auto">
                   <v-data-table
-                    :headers="batting_headers"
-                    :items="desserts"
-                    class="elevation-1"
-                    mobile-breakpoint="0"
-                    hide-default-footer
-                    disable-sort
-                    :items-per-page=-1
+                      :headers="batting_headers"
+                      :items="first_batting"
+                      class="elevation-1"
+                      mobile-breakpoint="0"
+                      hide-default-footer
+                      disable-sort
+                      :items-per-page="-1"
                   >
                     <template v-slot:[`item.name`]="{ item }">
                       {{ item.name }}
@@ -49,30 +60,80 @@
                     >
                       <tr>
                         <td :colspan="headers.length">
-                          Extras: 13 ( b 5 lb 1 nb 7 )
+                          Extras: {{first_batting_extra}}
                         </td>
                       </tr>
                       <tr>
                         <td :colspan="headers.length">
-                          Total: 128
+                          Total: {{first_batting_total}}
                         </td>
                       </tr>
                     </template>
                   </v-data-table>
-                  <v-divider class="pb-5"></v-divider>
+                    <v-divider class="pb-5"></v-divider>
+                    <v-data-table
+                      :headers="bowling_headers"
+                      :items="first_bowling"
+                      class="elevation-1"
+                      mobile-breakpoint="0"
+                      hide-default-footer
+                      disable-sort
+                      :items-per-page="-1"
+                    >
+                    </v-data-table>
+                </v-col>
+              </v-row>
+            </v-tab-item>
+  
+            <v-tab-item
+              v-if="second_batting_team"
+            >
+              <v-row class="pb-5">
+                <v-col md="6" class="mx-auto">
                   <v-data-table
-                    :headers="bowling_headers"
-                    :items="bowling"
-                    class="elevation-1"
-                    mobile-breakpoint="0"
-                    hide-default-footer
-                    disable-sort
-                    :items-per-page=-1
+                      :headers="batting_headers"
+                      :items="second_batting"
+                      class="elevation-1"
+                      mobile-breakpoint="0"
+                      hide-default-footer
+                      disable-sort
+                      :items-per-page="-1"
                   >
+                    <template v-slot:[`item.name`]="{ item }">
+                      {{ item.name }}
+                      <div class="text-caption font-weight-light grey--text">{{ item.status }}</div>
+                    </template>
+                    <template
+                      v-slot:[`body.append`]="{ headers }"
+                    >
+                      <tr>
+                        <td :colspan="headers.length">
+                          Extras: {{second_batting_extra}}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td :colspan="headers.length">
+                          Total: {{second_batting_total}}
+                        </td>
+                      </tr>
+                    </template>
                   </v-data-table>
-              </v-col>
-            </v-row>
-          </v-tab-item>
+                    <v-divider class="pb-5"></v-divider>
+                    <v-data-table
+                      :headers="bowling_headers"
+                      :items="second_bowling"
+                      class="elevation-1"
+                      mobile-breakpoint="0"
+                      hide-default-footer
+                      disable-sort
+                      :items-per-page="-1"
+                    >
+                    </v-data-table>
+                </v-col>
+              </v-row>
+            </v-tab-item>
+
+          </v-tabs-items>
 
           <v-tab-item>
           </v-tab-item>
@@ -84,198 +145,162 @@
 
 <script>
 
+import _ from 'lodash';
+import { doc, getDoc, collection, query, getDocs, orderBy } from "firebase/firestore";
+
+import db from '../db.js';
+
   export default {
     name: 'ScorecardView',
 
     components: {
     },
 
-    data () {
+    data: function () {
       return {
+        tab: null,
+        first_batting: [],
+        first_bowling: [],
+        second_batting: [],
+        second_bowling: [],
+        first_batting_team: null,
+        first_bowling_team: null,
+        second_batting_team: null,
+        second_bowling_team: null,
+        first_batting_extra: null,
+        first_batting_total: null,
+        second_batting_extra: null,
+        second_batting_total: null,
         batting_headers: [
-          { text: 'Batting',
-            align: 'start',
-            value: 'name',
-            width: '100%'
-          },
-          { text: 'R', value: 'calories', align: 'end'  },
-          { text: 'B', value: 'fat', align: 'end'  },
-          { text: '4s', value: 'carbs', align: 'end'  },
-          { text: '6s', value: 'protein', align: 'end'  },
-          { text: 'S/R', value: 'iron', align: 'end' },
+          { text: 'Batting', align: 'start', value: 'name', width: '100%'},
+          { text: 'R', value: 'score', align: 'end'  },
+          { text: 'B', value: 'balls', align: 'end'  },
+          { text: '4s', value: 'four', align: 'end'  },
+          { text: '6s', value: 'six', align: 'end'  },
+          { text: 'S/R', value: 'sr', align: 'end' },
         ],
         bowling_headers: [
-          { text: 'Bowling',
-            align: 'start',
-            value: 'name',
-            width: '100%'
-          },
-          { text: 'O', value: 'calories', align: 'end'  },
-          { text: 'M', value: 'fat', align: 'end'  },
-          { text: 'R', value: 'carbs', align: 'end'  },
-          { text: 'W', value: 'protein', align: 'end'  },
-          { text: 'Econ', value: 'iron', align: 'end' },
-        ],
-        desserts: [
-          {
-            name: 'P. Madubashana',
-            status: 'c J. Kaushal b N. Jayawardana',
-            calories: 22,
-            fat: 45,
-            carbs: 2,
-            protein: 0,
-            iron: '48.89',
-          },
-          {
-            name: 'H. Achintha',
-            status: 'b H. De Silva',
-            calories: 3,
-            fat: 10,
-            carbs: 0,
-            protein: 0,
-            iron: 30.0,
-          },
-          {
-            name: 'D. Kalupahana',
-            status: 'lbw b M. Tharupathi',
-            calories: 37,
-            fat: 100,
-            carbs: 4,
-            protein: 0,
-            iron: 37.0,
-          },
-          {
-            name: 'D. Induwara',
-            status: 'c H. De Silva b N. Jayawardana',
-            calories: 3,
-            fat: 10,
-            carbs: 0,
-            protein: 0,
-            iron: 30.0,
-          },
-          {
-            name: 'C. De Silva',
-            status: 'c H. De Silva b S. Abishek',
-            calories: 1,
-            fat: 15,
-            carbs: 0,
-            protein: 0,
-            iron: 6.67,
-          },
-          {
-            name: 'T. Dilshan',
-            status: 'c H. De Silva b S. Abishek',
-            calories: 0,
-            fat: 3,
-            carbs: 0,
-            protein: 0,
-            iron: 0.0,
-          },
-          {
-            name: 'H. Gallage',
-            status: 'c B. Manohara b N. Jayawardana',
-            calories: 39,
-            fat: 96,
-            carbs: 4,
-            protein: 0,
-            iron: 40.63,
-          },
-          {
-            name: 'R. Hettiarachchi',
-            status: 'c H. De Silva b S. Abishek',
-            calories: 0,
-            fat: 6,
-            carbs: 0,
-            protein: 0,
-            iron: 0.00,
-          },
-          {
-            name: 'S. Hasaranga',
-            status: 'lbw b N. Jayawardana',
-            calories: 9,
-            fat: 22,
-            carbs: 1,
-            protein: 0,
-            iron: 40.91,
-          },
-          {
-            name: 'K. Madusha',
-            status: 'run out(S. Abishek)',
-            calories: 2,
-            fat: 4,
-            carbs: 0,
-            protein: 0,
-            iron: 40.91,
-          },
-          {
-            name: 'K. Rukshan',
-            status: 'not out',
-            calories: 0,
-            fat: 3,
-            carbs: 0,
-            protein: 0,
-            iron: 0.00,
-          }
-        ],
-        bowling: [
-          {
-            name: 'K. Nethsara',
-            calories: 5,
-            fat: 7,
-            carbs: 0,
-            protein: 0,
-            iron: 1.40,
-          },
-          {
-            name: 'H. De. Silva',
-            calories: 5,
-            fat: 15,
-            carbs: 0,
-            protein: 1,
-            iron: 3.0,
-          },
-          {
-            name: 'T. Nirmal',
-            calories: 4,
-            fat: 19,
-            carbs: 0,
-            protein: 0,
-            iron: 4.75,
-          },
-          {
-            name: 'N. Jayawardana',
-            calories: 18.1,
-            fat: 20,
-            carbs: 0,
-            protein: 4,
-            iron: 1.10,
-          },
-          {
-            name: 'C. De Silva',
-            calories: 1,
-            fat: 15,
-            carbs: 0,
-            protein: 0,
-            iron: 6.67,
-          },
-          {
-            name: 'S. Abishek',
-            calories: 14,
-            fat: 38,
-            carbs: 0,
-            protein: 3,
-            iron: 2.71,
-          },
-          {
-            name: 'M. Tharupathi',
-            calories: 5,
-            fat: 24,
-            carbs: 0,
-            protein: 1,
-            iron: 4.80,
-          }
+          { text: 'Bowling', align: 'start', value: 'name', width: '100%'},
+          { text: 'O', value: 'overs', align: 'end'  },
+          { text: 'M', value: 'maiden', align: 'end'  },
+          { text: 'R', value: 'score', align: 'end'  },
+          { text: 'W', value: 'wickets', align: 'end'  },
+          { text: 'Econ', value: 'econ', align: 'end' },
         ],
       }
     },
+
+  async mounted(){
+
+    const configRef = doc(db, "main", "config");
+    const configSnap = await getDoc(configRef);
+
+    if (configSnap.exists()) {
+      this.first_batting_team = _.get(configSnap.data().inningOrder[0], 'team', null);
+      this.first_bowling_team = _.get(configSnap.data().inningOrder[0], 'team', null);
+      this.second_batting_team = _.get(configSnap.data().inningOrder[1], 'team', null);
+      this.second_bowling_team = _.get(configSnap.data().inningOrder[1], 'team', null);
+    }
+
+    const bowling_q = query(collection(db, "bowling"), orderBy("timestamp"));
+
+    const bowlingSnapshot = await getDocs(bowling_q);
+    bowlingSnapshot.forEach((doc) => {
+
+      var bowling_row = doc.data();
+      var overs = 0;
+      var econ = 0;
+      
+      if (doc.data().team === this.first_bowling_team && doc.data().inning === 1){
+
+        overs = Math.floor(doc.data().balls/6)+(doc.data().balls%6)/10;
+        bowling_row.overs = overs;
+
+        econ = doc.data().score/overs
+        bowling_row.econ = econ.toFixed(2);
+
+        this.first_bowling.push(bowling_row);
+      }
+
+      if (doc.data().team === this.second_bowling_team && doc.data().inning === 1){
+
+        overs = Math.floor(doc.data().balls/6)+(doc.data().balls%6)/10;
+        bowling_row.overs = overs;
+
+        econ = doc.data().score/overs
+        bowling_row.econ = econ.toFixed(2);
+
+        this.second_bowling.push(bowling_row);
+      }
+    });
+
+    const batting_q = query(collection(db, "batting"), orderBy("timestamp"));
+
+    const battingSnapshot = await getDocs(batting_q);
+    battingSnapshot.forEach((doc) => {
+
+      var batting_row = doc.data();
+      var sr = 0;
+      
+      if (doc.data().team === this.first_batting_team && doc.data().inning === 1){
+        sr = (doc.data().score/doc.data().balls)*100
+        batting_row.sr = sr.toFixed(2);
+        this.first_batting.push(batting_row);
+      }
+
+      if (doc.data().team === this.second_batting_team && doc.data().inning === 1){
+        sr = (doc.data().score/doc.data().balls)*100
+        batting_row.sr = sr.toFixed(2);
+        this.second_batting.push(batting_row);
+      }
+    });
+
+    const inning_q = query(collection(db, "innings"));
+
+    const inningSnapshot = await getDocs(inning_q);
+    inningSnapshot.forEach((doc) => {
+      if (doc.data().team === this.first_batting_team && doc.data().inning === 1){
+        this.first_batting_total = doc.data().score;
+
+        var extras = doc.data().extra.total + " ( "
+        if (doc.data().extra.b > 0){
+          extras = extras + "b "+doc.data().extra.b+" ";
+        }
+        if (doc.data().extra.lb > 0){
+          extras = extras + "lb " + doc.data().extra.lb + " "
+        }
+        if (doc.data().extra.nb > 0){
+          extras = extras + "nb " + doc.data().extra.nb + " "
+        }
+        if (doc.data().extra.wd > 0){
+          extras = extras + "wd " + doc.data().extra.wd + " "
+        }
+        extras = extras+")"
+
+        this.first_batting_extra = extras;
+      }
+      if (doc.data().team === this.second_batting_team && doc.data().inning === 1){
+        this.second_batting_total = doc.data().score;
+
+        extras = doc.data().extra.total + " ( "
+        if (doc.data().extra.b > 0){
+          extras = extras + "b "+doc.data().extra.b+" ";
+        }
+        if (doc.data().extra.lb > 0){
+          extras = extras + "lb " + doc.data().extra.lb + " "
+        }
+        if (doc.data().extra.nb > 0){
+          extras = extras + "nb " + doc.data().extra.nb + " "
+        }
+        if (doc.data().extra.wd > 0){
+          extras = extras + "wd " + doc.data().extra.wd + " "
+        }
+        extras = extras+")"
+
+        this.second_batting_extra = extras;
+      }
+    });
+
+  },
   }
 </script>
