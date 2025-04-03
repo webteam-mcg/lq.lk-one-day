70d817cff6a5b6c7afa4b569b6bac438ab79a0e8
Sandul Renuja <sandulrenuja@gmail.com>
2025-04-03 21:29:53 +0530

    Update registerServiceWorker.js to remove existing service-workers and clear cache

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,32 +1,48 @@
 /* eslint-disable no-console */
 
-import { register } from 'register-service-worker'
+// import { register } from 'register-service-worker'
 
-if (process.env.NODE_ENV === 'production') {
-  register(`${process.env.BASE_URL}service-worker.js`, {
-    ready () {
-      console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB'
-      )
-    },
-    registered () {
-      console.log('Service worker has been registered.')
-    },
-    cached () {
-      console.log('Content has been cached for offline use.')
-    },
-    updatefound () {
-      console.log('New content is downloading.')
-    },
-    updated () {
-      console.log('New content is available; please refresh.')
-    },
-    offline () {
-      console.log('No internet connection found. App is running in offline mode.')
-    },
-    error (error) {
-      console.error('Error during service worker registration:', error)
-    }
-  })
+// if (process.env.NODE_ENV === 'production') {
+//   register(`${process.env.BASE_URL}service-worker.js`, {
+//     ready () {
+//       console.log(
+//         'App is being served from cache by a service worker.\n' +
+//         'For more details, visit https://goo.gl/AFskqB'
+//       )
+//     },
+//     registered () {
+//       console.log('Service worker has been registered.')
+//     },
+//     cached () {
+//       console.log('Content has been cached for offline use.')
+//     },
+//     updatefound () {
+//       console.log('New content is downloading.')
+//     },
+//     updated () {
+//       console.log('New content is available; please refresh.')
+//     },
+//     offline () {
+//       console.log('No internet connection found. App is running in offline mode.')
+//     },
+//     error (error) {
+//       console.error('Error during service worker registration:', error)
+//     }
+//   })
+// }
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    registrations.forEach(registration => {
+      registration.unregister();
+      console.log('Service worker unregistered.');
+    });
+  });
+
+  caches.keys().then(cacheNames => {
+    cacheNames.forEach(cacheName => {
+      caches.delete(cacheName);
+      console.log(`Cache ${cacheName} deleted.`);
+    });
+  });
 }
